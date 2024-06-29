4ccb353299cb9d929f3c00d60459aa98cd265bb9
Mateus0512 <ax7maf@gmail.com>
2024-06-29 12:22:07 -0300

    Atualização Cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import fastify from "fastify";
-// import fastifyCors from "@fastify/cors";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify();
 
-// app.register(fastifyCors,{
-//     origin: '*'
-// });
+app.register(fastifyCors,{
+    origin: '*'
+});
 
 app.get('/linhas/:nome?', async(request,reply)=>{
     if(request.params){
